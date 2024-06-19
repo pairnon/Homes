90fd90dbb14bd9367755607f603ef3ed398457eb
pairnon <janpairnon@gmail.com>
2024-06-18 20:42:33 -0700

    Add Main class

---
+++ app/src/main/java/io/papermc/homes/Main.java
@@ -0,0 +1,22 @@
+package io.papermc.homes;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+    private static Main plugin;
+
+    public static Main getPlugin() {
+        return plugin;
+    }
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        plugin = this;
+
+    }
+    
+}
