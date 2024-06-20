0185a8b6219b9bcf59a27e5916d0185af852b029
pairnon <janpairnon@gmail.com>
2024-06-19 22:31:06 -0700

    Add player messaging methods

--- app/src/main/java/io/papermc/homes/Broadcasting.java
+++ app/src/main/java/io/papermc/homes/Broadcasting.java
@@ -2,6 +2,7 @@ package io.papermc.homes;
 
 import org.bukkit.Sound;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -9,6 +10,21 @@ import net.kyori.adventure.text.format.NamedTextColor;
 public class Broadcasting {
 
     public static final Sound MESSAGE_SOUND = Sound.BLOCK_NOTE_BLOCK_SNARE;
+
+    public static void sendMessage(Player p, String message) {
+        p.sendMessage(Component.text(message, NamedTextColor.GOLD));
+        p.playSound(p.getLocation(), MESSAGE_SOUND, 1.0F, 1.0F);
+    }
+
+    public static void sendSuccessMessage(Player p, String message) {
+        p.sendMessage(Component.text(message, NamedTextColor.GREEN));
+        p.playSound(p.getLocation(), MESSAGE_SOUND, 1.0F, 1.0F);
+    }
+    
+    public static void sendErrorMessage(Player p, String message) {
+        p.sendMessage(Component.text(message, NamedTextColor.RED));
+        p.playSound(p.getLocation(), MESSAGE_SOUND, 1.0F, 1.0F);
+    }
 
     public static void sendMessageResponse(CommandSender c, String message) {
         c.sendMessage(Component.text(message, NamedTextColor.GOLD));
