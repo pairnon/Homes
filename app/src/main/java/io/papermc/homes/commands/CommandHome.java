41bf692367f50f2425b9ad715cc4dc3ce3bd4f89
pairnon <janpairnon@gmail.com>
2024-06-19 22:28:53 -0700

    Add check for invalid executor

--- app/src/main/java/io/papermc/homes/commands/CommandHome.java
+++ app/src/main/java/io/papermc/homes/commands/CommandHome.java
@@ -3,6 +3,7 @@ package io.papermc.homes.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import io.papermc.homes.Broadcasting;
 
@@ -10,6 +11,11 @@ public class CommandHome implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            Broadcasting.sendErrorResponse(sender, "You must be a player to execute this command.");
+            return true;
+        }
 
         if (args.length == 0) {
             Broadcasting.sendErrorResponse(sender, "You must specify a home.");
